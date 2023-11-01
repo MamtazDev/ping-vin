505b11db0027f5d64adf73ce046c2ce8feec45ba
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2023-11-01 18:33:42 +0600

    BrandProtection Done

--- src/components/OurProduct/OurProduct.jsx
+++ src/components/OurProduct/OurProduct.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { ourProductData } from './../../data/data';
+import './OurProduct.css'
 
 const OurProduct = () => {
     return (
         <section className='our_product'>
-            OurProduct
+            <div className="container">
+                <div className="section_heading text-center pb_60">
+                    <h1 className="about_text mb-4">
+                        Our <span className="yellow">Product</span>
+                    </h1>
+
+                    <p className="fs-6 fw-medium">
+                        We have built a moderation engine that can detect millions of illegal websites without needing a human to look at disturbing <br /> content. This engine also helps our highly trained moderators to filter out user generated content which makes them faster <br /> and more accurate than a moderator without the engine.
+                    </p>
+                </div>
+                <div className="row">
+                    {
+                        ourProductData.map((productInfo, index) => (
+                            <div className='col-lg-4' key={index}>
+                                <div className="product_card">
+                                    <div>
+                                        <img src={productInfo.icon} alt="icon" />
+                                    </div>
+                                    <h4>
+                                        {productInfo.productName}
+                                    </h4>
+                                    <p>
+                                        {
+                                            productInfo.productInfo
+                                        }
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+
+
+            </div>
+
         </section>
     )
 }
---
+++ src/data/data.jsx
@@ -0,0 +1,21 @@
+import productIconOne from '../assets/product1.svg'
+import productIconTwo from '../assets/product2.svg'
+import productIconThree from '../assets/product3.svg'
+
+export const ourProductData = [
+    {
+        icon: productIconOne,
+        productName: 'Moderation Engine',
+        productInfo: 'The core of the product is a sophisticated moderation engine. This engine is designed to scan and analyze websites, particularly focusing on identifying and flagging illegal content. It likely uses a combination of algorithms, machine learning, and human oversight to achieve this.'
+    },
+    {
+        icon: productIconTwo,
+        productName: 'Moderation Engine',
+        productInfo: 'The core of the product is a sophisticated moderation engine. This engine is designed to scan and analyze websites, particularly focusing on identifying and flagging illegal content. It likely uses a combination of algorithms, machine learning, and human oversight to achieve this.'
+    },
+    {
+        icon: productIconThree,
+        productName: 'Moderation Engine',
+        productInfo: 'The core of the product is a sophisticated moderation engine. This engine is designed to scan and analyze websites, particularly focusing on identifying and flagging illegal content. It likely uses a combination of algorithms, machine learning, and human oversight to achieve this.'
+    },
+]
