02e0179ea4e6f56b5cea6a88fdba24028cf80d10
tonni-akter-dev <tanniakhter123@gmail.com>
2023-11-01 16:55:16 +0600

    update

---
+++ src/components/BrandProtectionHome/BrandProtectionHome.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import brand from '../../assets/brand.png'
+const BrandProtectionHome = () => {
+  return (
+    <>
+     <div className="container brand_bg">
+     <h1 className="about_text ">
+        Brand <span className="yellow">Protection</span>
+      </h1>
+      <p>
+        Think your brand or products appears somewhere it should not be? We can
+        search and locate your specific brand in thousands of websites using our
+        in-house moderation tool and moderators.
+      </p>
+     </div>
+    </>
+  );
+};
+
+export default BrandProtectionHome;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Banner from "../Banner/Banner";
 import About from "../About/About";
 import Consultancy from "../Consultancy/Consultancy";
+import BrandProtectionHome from "../BrandProtectionHome/BrandProtectionHome";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <About />
-      <Consultancy/>
+      <Consultancy />
+      <BrandProtectionHome />
     </>
   );
 };
