aa68d8599db22f0b5a09a469f6dfb66ce9d2139b
tonni-akter-dev <tanniakhter123@gmail.com>
2023-11-02 15:16:59 +0600

    update product page

--- src/pages/BrandProtection/BrandProtection.jsx
+++ src/pages/BrandProtection/BrandProtection.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './BrandProtection.css'
+import PageHeader from './../PageHeader/PageHeader';
 const BrandProtection = () => {
     return (
         <>
+        <PageHeader/>
+        <div className="container">
+            
+        </div>
             
         </>
     );
