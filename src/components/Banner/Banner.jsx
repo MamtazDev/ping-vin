f4737cbf40b881d8ca4305dc8f3088da2481aa60
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2023-11-02 17:19:16 +0600

    : update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     <section className="banner_bg">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-lg-6 mb-5 ">
+          <div className="col-lg-6 mb-5 " data-aos="fade-right">
             <h1 className="fs_65 banner_title white_color mb_30">
               The start-up specialized in mass
               <span className="yellow"> moderation.</span>
@@ -25,7 +25,7 @@ const Banner = () => {
             <ContactUsButton />
           </div>
 
-          <div className="col-lg-5 offset-lg-1 banner_right">
+          <div className="col-lg-5 offset-lg-1 banner_right " data-aos="fade-left">
             <h3 className="fs_32">Moderering Beta</h3>
             <input type="text" /> <br />
             <button className="fs_16 black2 search_btn ">Search</button>
