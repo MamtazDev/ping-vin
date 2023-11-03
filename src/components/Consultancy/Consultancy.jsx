3e6cc4760913acdc09ef461859ef9455db3d8b59
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2023-11-03 10:14:37 +0600

    : Update

--- src/components/Consultancy/Consultancy.jsx
+++ src/components/Consultancy/Consultancy.jsx
@@ -15,25 +15,25 @@ const Consultancy = () => {
               <h2 className="fs_48 about_text blue" data-aos="fade-up" data-aos-delay="300">Consultancy</h2>
               <div className="consultancy_small_text black" data-aos="fade-up">
                 <p data-aos="fade-up" data-aos-delay="400">
-                <img src={yellowicon} className="me-1" alt="" /> Helping Private
-                and Governmental agencies assess the legality of a website.
-              </p>
-              <p data-aos="fade-up" data-aos-delay="500">
-                <img src={yellowicon} className="me-1" alt="" /> Providing Site
-                checks and consulting services.
-              </p>
-              <p data-aos="fade-up" data-aos-delay="600">
-                <img src={yellowicon} className="me-1" alt="" /> Go to the
-                contact page to get in touch with the correct service.
-              </p>
-            </div>
-            <ContactUsButton />
+                  <img src={yellowicon} className="me-1" alt="" /> Helping Private
+                  and Governmental agencies assess the legality of a website.
+                </p>
+                <p data-aos="fade-up" data-aos-delay="500">
+                  <img src={yellowicon} className="me-1" alt="" /> Providing Site
+                  checks and consulting services.
+                </p>
+                <p data-aos="fade-up" data-aos-delay="600">
+                  <img src={yellowicon} className="me-1" alt="" /> Go to the
+                  contact page to get in touch with the correct service.
+                </p>
+              </div>
+              <ContactUsButton />
             </div>
           </div>
           <div className="col-lg-6">
 
             <div className="consultancy_right mb-4 mb-lg-0" data-aos="fade-left">
-              <img className="img-fluid" src={c1} alt="" />
+              <img className="w-100" src={c1} alt="" />
 
               <div className="time_design">
                 <h1>20+</h1>
@@ -41,17 +41,17 @@ const Consultancy = () => {
               </div>
 
               <div className="row mt-3 pe-0 ps-0">
-                <div className="col-lg-6 pe-0 ps-0 ps-lg-3 ">
+                <div className="col-lg-6 pe-0 ps-0">
                   <div className="mb-4 mb-lg-0 text-center">
 
-                  <img className="img-fluid" src={c2} alt="" />
+                    <img className="img-fluid" src={c2} alt="" />
                   </div>
                 </div>
 
                 <div className="col-lg-6 pe-0 ps-0 ">
                   <div className="mb-4 mb-lg-0 text-center">
 
-                  <img className="img-fluid" src={c3} alt="" />
+                    <img className="img-fluid" src={c3} alt="" />
                   </div>
                 </div>
               </div>
